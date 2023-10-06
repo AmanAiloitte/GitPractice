5d6d00fe374aa4592ba25da834425ac57fd31e07
AmanAiloitte <aman@ailoitte.com>
2023-10-06 13:11:50 +0530

    Added script file and functionality of submit button

---
+++ script.js
@@ -0,0 +1,10 @@
+function submit() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  document.getElementById("showEmail").innerHTML = email;
+  document.getElementById("showPassword").innerHTML = password;
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+}
+
+function reset() {}
